fd5156a8e1bf4ab5e3aa4f738d78c30d10b9c58b
Tiago Farias <tiago.costa@ati.to.gov.br>
2020-12-10 22:54:44 -0300

    component

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,29 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 
 import './styles.css';
 import api from '../../services/api';
+import { Progress } from 'reactstrap';
+//import { ToastContainer, toast } from 'react-toastify';
+//import 'react-toastify/dist/ReactToastify.css';
 
 function Landing() {
-  const [totalConnections, setTotalConnections] = useState(0);
 
-  useEffect(() => {
-    api.get('connections').then(response => {
-      const { total } = response.data;
-      setTotalConnections(total);
-    });
-  }, []);
+  this.state = {
+    selectedFile: null,
+    loaded: 0
+  }
+
+  onChangeHandler = event => {
+    var files = event.target.files
+    // if return true allow to setState
+    this.setState({
+      selectedFile: files,
+      loaded: 0
+    })
+  }
+
+  onClickHandler = () => {
+    const data = new FormData()
+    data.append('file', this.state.selectedFile)
+
+    api.post("api/file/uploadFile", data, {
+      onUploadProgress: ProgressEvent => {
+        this.setState({
+          loaded: (ProgressEvent.loaded / ProgressEvent.total * 100),
+        })
+      },
+    })
+      .then(res => { // then print response status
+        console.log(res);
+        //toast.success('upload success')
+      })
+      .catch(err => { // then print response status
+        //toast.error('upload fail')
+      })
+  };
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
 
         <div className="logo-container">
 
-          <h2>Sua plataforma de estudos online</h2>
+          <h2>Sua plataforma de doação de sangue</h2>
         </div>
 
+        <div class="row">
+          <div class="offset-md-3 col-md-6">
+            <div class="form-group files">
+              <label>Upload Your File </label>
+              <input type="file" class="form-control" onChange={this.onChangeHandler} />
+            </div>
+            <div class="form-group">
+              {/* <ToastContainer /> */}
+              <Progress max="100" color="success" value={this.state.loaded} >{Math.round(this.state.loaded, 2)}%</Progress>
+
+            </div>
+
+            <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
+
+          </div>
+
+        </div>
       </div>
-    </div>
+      </div>
   )
 }
 
